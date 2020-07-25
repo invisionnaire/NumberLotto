abf4c17359b85cf635cdd99a1072ac015d2dccb3
Invisionnaire <arosairo@invisionnaire.com>
2020-07-25 14:14:01 -0500

    modals are fix and the JS code is was refractor to be shorter

--- js/main.js
+++ js/main.js
@@ -10,51 +10,46 @@ function gameProperties (){
 
 
 function preGame() {
-
     // let gameLevel = Math.floor(Math.random() * 5 + 1);
     let gameLevel = 1; // dev var, remove or comment when done.
-
     if (gameLevel === 1) {
         selectedNumber = Math.floor(Math.random() * 10 + 1);
         gameProperties()
 
-        
+        gameTry = '';
+        for (let t = 1; t <= 2; t++) {
+            gameTry += ` <i class="fas fa-hexagon"></i> `;
+            };
 
+        triesRemain ='';
+        for (let n = 1; n <= 2; n++) {
+            triesRemain = 1;
+            };
+            
         // print to html
         document.querySelector('#game_level').innerHTML = 1;
         document.querySelector('#maxNum').innerHTML = maxNumber;
+        document.querySelector('#strikes').innerHTML = gameTry;
+        document.querySelector('#triesRemain').innerHTML = triesRemain;
+
     }
 };
 
+preGame()
 
 function startGame() {
-
     numberInp = document.getElementById('inputNumber').value;
-
     if (selectedNumber === +numberInp) {
         // alert('You win');
-        $("#correctModal").modal()
-
+        $("#correctModal").modal();
     } else {
         // alert('You Lose');
         $("#wrongModal").modal()
     };
-
     document.querySelector('#numberSelected').innerHTML = selectedNumber;
     document.querySelector('#selectedNum').innerHTML = selectedNumber;
-
-
-
 };
 
 function reload1() {
     location.reload();
 };
-
-
-
-
-
-//  Something goes here
-
-preGame()
